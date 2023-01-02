ba5f98160fa7896b96988ceccefc097496dfca93
Shihab <imsahariashihab+github@gmail.com>
2023-01-02 15:28:27 +0600

    Init Project && Adding src/ and other files

---
+++ README.md
@@ -0,0 +1,22 @@
+# useSpace
+a simple library to use localStorage
+
+# Installation
+```
+npm install esmls
+```
+
+```js
+import {get, set, has, del} from "esmls";
+set("isLoaded", false);
+
+window.addEventListener("DOMContentLoaded", () => {
+    set("isLoaded", true);
+});
+
+document.addEventListener("click", (e)=>{
+    if(!get("isLoaded")){
+        alert("wait to load the page");
+    }
+});
+```
---
+++ src/index.ts
@@ -0,0 +1,86 @@
+/**
+ * @type {Storage}
+ */
+const LOCAL__STORAGE: Storage = window.localStorage;
+/**
+ * @param {any} data
+ */
+function type(data: any): string {
+    // @ts-ignore
+    return Object.prototype.toString.call(data).match(/\[object (.*)\]/)[1];
+}
+
+/**
+ * @param {string} error
+ */
+function throwError(error: string) {
+    throw new Error(error);
+}
+/**
+ * @param {string} key
+ */
+function isKey(key: string) {
+    if (!key) return throwError("key is required");
+    if (typeof key !== "string") return throwError("key must be string");
+}
+
+/**
+ *  @param {string} key
+ *  @param {any} data
+ */
+function setItem(key: string, data: any): void {
+    isKey(key);
+    LOCAL__STORAGE.setItem(
+        key,
+        JSON.stringify({
+            type: type(data),
+            data: data,
+        })
+    );
+}
+/**
+ * @param {string} key - get data from `LocalStorage`
+ * @param {any} defitem - pass a default item if there is no item in `LocalStorage`
+ */
+function getItem(key: string, defitem?: any): any {
+    isKey(key);
+    if (!hasItem(key)) {
+        return defitem ? defitem : null;
+    }
+    // @ts-ignore
+    const ReadStorage = JSON.parse(LOCAL__STORAGE.getItem(key));
+    if (ReadStorage.type === "String") {
+        return String(ReadStorage.data);
+    } else if (ReadStorage.type === "Number") {
+        return Number(ReadStorage.data);
+    } else if (ReadStorage.type === "BigInt") {
+        return BigInt(ReadStorage.data);
+    } else if (ReadStorage.type === "Boolean") {
+        return Boolean(ReadStorage.data);
+    } else if (ReadStorage.type === "Date") {
+        return new Date(ReadStorage.data);
+    } else {
+        return ReadStorage.data ? ReadStorage.data : ReadStorage;
+    }
+}
+/**
+ * @param {string} key - delete item from `LocalStorage`
+ */
+function delItem(key: string) {
+    isKey(key);
+    LOCAL__STORAGE.removeItem(key);
+}
+/**
+ * @param {string} key - if `LocalStorage` has the item you're looking for..
+ */
+function hasItem(key: string) {
+    isKey(key);
+    return LOCAL__STORAGE.hasOwnProperty(key);
+}
+
+export default {
+    set: setItem,
+    get: getItem,
+    del: delItem,
+    has: hasItem
+}
